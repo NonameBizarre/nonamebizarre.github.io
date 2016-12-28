571555272173996d079d80793e12503434eed536
NonameBizarre <CyrilBelov@yandex.ru>
2016-12-29 01:54:23 +0300

    Update Gameplay.js

--- HTML5Test/Reaction/js/game/states/Gameplay.js
+++ HTML5Test/Reaction/js/game/states/Gameplay.js
@@ -17,13 +17,27 @@ var Game;
             this._plank = this.game.add.image(this.world.width / 2, this.world.height / 2, 'bgPlane');
             this._plank.anchor.set(0.5);
             this._GameElementContainer.addChild(this._plank);
-            this.player = this.game.add.image(this.world.width / 2, this.world.height / 2, 'player');
-            this.player.anchor.set(0.5);
-            this._GameElementContainer.addChild(this.player);
-            this.player.inputEnabled = true;
-            this.player.input.enableDrag();
+            this._player = this.game.add.image(this.world.width / 2, this.world.height / 2, 'player');
+            this._player.anchor.set(0.5);
+            this._GameElementContainer.addChild(this._player);
+            this._player.inputEnabled = true;
+            this._player.input.enableDrag();
+            //this._player.body.velocity.x = -100;
         };
         Gameplay.prototype.update = function () {
+            if (!this.checkOverlap(this._plank, this._player)) {
+                this.game.stage.backgroundColor = 0x992d2d;
+            }
+            else {
+                this.game.stage.backgroundColor = 0xffcc00;
+            }
+        };
+        Gameplay.prototype.checkOverlap = function (obj1, obj2) {
+            var obj2 = obj2.getBounds();
+            var obj1 = new Phaser.Rectangle(0, 0, this._plank.width - this._player.width * 2, this._plank.height - this._player.height * 2); //obj2.getBounds();
+            obj1.x = this._plank.x - this._plank.width / 2 + this._player.width;
+            obj1.y = this._plank.y - this._plank.height / 2 + this._player.height;
+            return Phaser.Rectangle.intersects(obj1, obj2);
         };
         return Gameplay;
     }(Phaser.State));
