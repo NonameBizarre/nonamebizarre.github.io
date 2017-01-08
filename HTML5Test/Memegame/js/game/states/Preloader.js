37cd4c4d923fcf859d0fe7f1b299478304be7a52
NonameBizarre <CyrilBelov@yandex.ru>
2017-01-08 22:34:52 +0300

    Add files via upload

--- HTML5Test/Memegame/js/game/states/Preloader.js
+++ HTML5Test/Memegame/js/game/states/Preloader.js
@@ -15,9 +15,11 @@ var Game;
             this.game.load.onFileComplete.add(this.LoadingUpdate, this);
             //  Загрузка ассетов
             //this.game.load.atlas( 'gameplay', "assets/atlass/gameplay.png", "assets/atlass/gameplay.json" );
-            this.game.load.image('CellClean', 'assets/CellClean.png');
+            //this.game.load.image('CellClean', 'assets/CellClean.png');
+            this.game.load.image('CellClean', 'assets/CellClean2.png');
             this.game.load.image('TableClean', 'assets/TableClean.png');
             //Игровые объекты
+            /*
             this.game.load.image('CellHat', 'assets/CellHat.png');
             this.game.load.image('CellBat', 'assets/CellBat.png');
             this.game.load.image('CellCoCoCo', 'assets/CellCoCoCo.png');
@@ -26,6 +28,7 @@ var Game;
             this.game.load.image('CellMOG', 'assets/CellMOG.png');
             this.game.load.image('CellPePe', 'assets/CellPePe.png');
             this.game.load.image('CellPommid', 'assets/CellPommid.png');
+
             this.game.load.image('DuDec', 'assets/DuDec.png');
             this.game.load.image('DyBovSky', 'assets/DyBovSky.png');
             this.game.load.image('GodBen', 'assets/GodBen.png');
@@ -38,6 +41,28 @@ var Game;
             this.game.load.image('NoFact', 'assets/NoFact.png');
             this.game.load.image('PhillPhish', 'assets/PhillPhish.png');
             this.game.load.image('Smoke', 'assets/Smoke.png');
+            */
+            //Игровые объекты
+            this.game.load.image('CellHat', 'assets/ammonite.png');
+            this.game.load.image('CellBat', 'assets/angler-fish.png');
+            this.game.load.image('CellCoCoCo', 'assets/bear-head.png');
+            this.game.load.image('CellGamepad', 'assets/chicken.png');
+            this.game.load.image('CellJoyS', 'assets/dolphin.png');
+            this.game.load.image('CellMOG', 'assets/duck.png');
+            this.game.load.image('CellPePe', 'assets/eagle-emblem.png');
+            this.game.load.image('CellPommid', 'assets/elephant-head.png');
+            this.game.load.image('DuDec', 'assets/feline.png');
+            this.game.load.image('DyBovSky', 'assets/gecko.png');
+            this.game.load.image('GodBen', 'assets/octopus.png');
+            this.game.load.image('hehehe', 'assets/parrot-head.png');
+            this.game.load.image('Kama', 'assets/polar-bear.png');
+            this.game.load.image('Kanobu', 'assets/rabbit.png');
+            this.game.load.image('KotLetKi', 'assets/snail.png');
+            this.game.load.image('Kovrizjka', 'assets/snake.png');
+            this.game.load.image('Mouse', 'assets/sperm-whale.png');
+            this.game.load.image('NoFact', 'assets/squid-head.png');
+            this.game.load.image('PhillPhish', 'assets/swan.png');
+            this.game.load.image('Smoke', 'assets/tiger.png');
         };
         Preloader.prototype.create = function () {
             console.log("Preloader");
